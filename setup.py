05e005dca0cc61e8cf54f358903371f30c41193c
wedwedwed495 <booth-cross-clammy@duck.com>
2025-04-03 17:56:38 +0200

    Update and rename setup.py to test_moduels.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='requests',
-    version='2.32.3',
-    packages=find_packages()
-)
---
+++ test_moduels.py
@@ -0,0 +1,7 @@
+try:
+ import http.client
+ import json
+ import time
+ import urllib.request
+except:
+    print('error with the moduels')
