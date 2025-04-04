9a9987a56c8973a8fbab19c1850ad4b759f5c814
wedwedwed495 <booth-cross-clammy@duck.com>
2025-04-04 09:49:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
-f = open('README.md', 'r')
-discryption = f.read
+
+with open('/Users/rolf/Downloads/README.md', 'r') as f:
+    global description
+    description = f.read()
 
 setup(
-  name = "requestt_pro"
-  version = 0.0.5
-  python_requires = '>=3.11.1'
+    name="requestt_pro",  
+    version="0.0.5",
+    python_requires='>=3.11.1',
+    url="https://github.com/wedwedwed495/wedwedwed495",
+    description="A brief description of your package",
+    long_description=description,  
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
 )
